e121bf77ed552e6259273367484a54ef133a9852
Tejal <tejalchaudhari89@gmail.com>
2020-10-05 21:30:41 +0530

    Renamed main controller

---
+++ ShopBridge/ShopBridge/Controllers/ShopBridgeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShopBridgeServices;
+using ShopBridgeServices.Interfaces;
+
+namespace ShopBridge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopBridgeController : ControllerBase
+    {
+        private readonly ILogger<ShopBridgeController> _logger;
+        
+        public ShopBridgeController(ILogger<ShopBridgeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("ShopBridge Api Started...");
+        }
+    }
+}
